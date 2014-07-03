498c2e298da70e5a3f492ed3c9f42f260e39b0ce
Eric Nelson <eric@sandalsoft.com>
2014-07-03 22:00:39 +0000

    Add component ticker-info

---
+++ ui/ticker-info.reel/ticker-info.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/ticker-info.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class TickerInfo
+ * @extends Component
+ */
+exports.TickerInfo = Component.specialize(/** @lends TickerInfo# */ {
+    constructor: {
+        value: function TickerInfo() {
+            this.super();
+        }
+    }
+});
